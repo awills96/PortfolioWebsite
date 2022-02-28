e2fd7d2ef1c2fbf947895347dc1e6179fad3283f
awills96 <vtalex2014@gmail.com>
2022-02-28 12:36:43 -0500

    started javascript coding

---
+++ resources/js/input.js
@@ -0,0 +1,8 @@
+let projectList = [
+    {
+        "image-path" : "resources/images/crafts.PNG",
+        "title" : "Dasmoto's Arts and Crafts",
+        "desc" : "This is a test website that I created based off a codeacademy prompt. The goal of this project was to learn fundamental HTML and CSS functions to a minimally guided project.",
+        "link" : "https://github.com/awills96/crafts-website-project"
+    }
+]
